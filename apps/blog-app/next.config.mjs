f43bfd8a2777a9c03e0902295188ff2d14be83ac
jagadish-varanasi <jagadishvaranasi123@gmail.com>
2024-11-12 10:20:27 +0530

    - Add chabges for env

--- apps/blog-app/next.config.mjs
+++ apps/blog-app/next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}/api/auth/creation`,
+  },
 };
 
 export default nextConfig;
