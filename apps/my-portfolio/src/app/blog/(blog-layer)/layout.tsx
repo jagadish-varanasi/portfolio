de9738c30339ba4b3ff8bcc86ec6a6a114b8aa40
jagadish-varanasi <jagadishvaranasi123@gmail.com>
2024-11-16 15:56:09 +0530

    - Added changes for layout

--- apps/my-portfolio/src/app/blog/(blog-layer)/layout.tsx
+++ apps/my-portfolio/src/app/blog/(blog-layer)/layout.tsx
@@ -23,7 +23,7 @@ function BlogDetails({
       </div>
       <aside className="md:w-[240px] lg:w-[300px] shrink-0">
         <div className="space-y-6">
-          <SliderDesign />
+          <SliderDesign showTitle={true} />
           <Promotion />
           <HobbyCard />
           <CodingProfile />
